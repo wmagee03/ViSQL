18113b3f92aec6ef076400fbb8672fb3b7bcbf77
William Magee <Will@Williams-MacBook-Pro.local>
2019-08-20 16:13:50 -0400

    edited gitignore

--- client/sqlReducer.js
+++ client/sqlReducer.js
@@ -12,16 +12,29 @@ const initState = {
 
 const SQLReducer = (state = initState, action) => {
 
+    let DB
+    let Tables;
+    let Table_Schemas;
     switch(action.type) {
         case types.ADD_DB:
-            const newDB = state.DB;
-            newDB[action.payload] = [];
+            DB = JSON.parse(JSON.stringify(state.DB));
+            DB[action.payload] = [];
 
             return {
                 ...state,
-                newDB,
+                DB,
             };
-            
+        
+        case types.ADD_TABLE:
+            DB = JSON.parse(JSON.stringify(state.DB));
+            Tables = state.Tables.slice();
+            Table_Schemas = JSON.parse(JSON.stringify(state.Table_Schemas));
+            const table = [];
+
+            if (DB[action.payload.dbName] === undefined) { return initState; }
+            DB[action.payload.dbName].push(action.payload.tableName);
+            Tables[action.payload.tableName] = [];
+
         default:
             return initState;
     }
