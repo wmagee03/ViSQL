6d8d08870a209f8b550f3ea2e91b8671da093be5
William Magee <Will@Williams-MacBook-Pro.local>
2019-08-20 15:18:59 -0400

    started work on sqlReducer

--- client/sqlReducer.js
+++ client/sqlReducer.js
@@ -1,1 +1,29 @@
 // REDUCER FOR SQL ACTION FUNCTIONS
+
+// import action creators
+import * as types from './actionTypes.js';
+
+const initState = {
+    DB: {},
+    Tables: {},
+    Table_Schemas: {},
+    Saved: true,
+};
+
+const SQLReducer = (state = initState, action) => {
+
+    switch(action.type) {
+        case types.ADD_DB:
+            const newDB = state.DB;
+            newDB[action.payload] = [];
+
+            return {
+                ...state,
+                newDB,
+            };
+            
+        default:
+            return initState;
+    }
+
+};
